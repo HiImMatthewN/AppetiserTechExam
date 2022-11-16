0f4db3e6457d4bf2d023491c8754a332bccaaa27
HiImMatthewN <matthewnantes33@gmail.com>
2022-11-17 01:59:37 +0800

    Add Full Screen

--- app/src/main/java/com/nantesmatthew/appetisertechexam/MainActivity.kt
+++ app/src/main/java/com/nantesmatthew/appetisertechexam/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.nantesmatthew.appetisertechexam
 
+import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.nantesmatthew.appetisertechexam.databinding.ActivityMainBinding
 import com.nantesmatthew.movie.presentation.MoviesFragmentDirections
@@ -16,20 +18,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binder: ActivityMainBinding
     private val viewModelMain by viewModels<MainViewModel>()
-    companion object{
+
+    companion object {
         val IS_SCREEN_RESTORED = "IsScreenRestored"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        fullScreen()
+
         super.onCreate(savedInstanceState)
 
         binder = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binder.root)
 
 
-
         //Only Restore Last Visited on startup/restore
         //Check if there's a saved state to prevent restoring on rotating
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             viewModelMain.getLastUserSession { userSession ->
                 when (userSession.screenOpened) {
                     Screen.Category -> {
@@ -53,15 +58,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
         //Save state on restore
-        outState.putBoolean(IS_SCREEN_RESTORED,true)
+        outState.putBoolean(IS_SCREEN_RESTORED, true)
     }
 
+
+    private fun fullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+    }
 
 }
